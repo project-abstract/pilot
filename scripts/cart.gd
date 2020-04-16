7f3e5d3d00a38fad4b21b0b0788a54d2976c3bca
Abiral Pokharel <22109745+abiral001@users.noreply.github.com>
2020-04-16 19:49:18 +0545

    Folder Changes

--- scripts/cart.gd
+++ scripts/cart.gd
@@ -4,35 +4,27 @@ var direction = Vector3()
 var speed = 200
 var velocity = Vector3()
 var angle = 0
-var g = -9.8
 
 func _physics_process(delta):
 	direction = Vector3(0,0,0)
 	if Input.is_key_pressed(KEY_UP):
-		direction.z -= 10*cos(deg2rad(angle*delta))
-		direction.x -= 10*sin(deg2rad(angle*delta))
+		direction.z -= 10*cos(deg2rad(angle))
+		direction.x -= 10*sin(deg2rad(angle))
 	if Input.is_key_pressed(KEY_DOWN):
-		direction.z += 10*cos(deg2rad(angle*delta))
-		direction.x += 10*sin(deg2rad(angle*delta))
+		direction.z += 10*cos(deg2rad(angle))
+		direction.x += 10*sin(deg2rad(angle))
 	if Input.is_key_pressed(KEY_LEFT):
 		#direction.x -= 10
 		angle += 10
-		rotate_y(deg2rad(10*delta))
+		rotate_y(deg2rad(10))
 	if Input.is_key_pressed(KEY_RIGHT):
 		#direction.x += 10
 		angle -= 10
-		rotate_y(deg2rad(-10*delta))
+		rotate_y(deg2rad(-10))
 	direction = direction.normalized()
 	direction = direction * speed * delta
-	#velocity.y += g*delta
 	velocity.x = direction.x
 	velocity.z = direction.z
-#	if velocity.y > 0:
-#		g = -29.4
-#	else:
-#		g = -9.8
-	
-	#move_and_collide(velocity)
 	velocity = move_and_slide(velocity, Vector3(0,-1,0))
 	
 	if angle == -10 :
@@ -41,5 +33,7 @@ func _physics_process(delta):
 		angle = 0
 	
 	if Input.is_key_pressed(KEY_SPACE):
-		print(velocity, delta)
+		print('Angle:rad deg',deg2rad(angle),'+' ,angle)
+		print('Angle: cos sin',cos(deg2rad(angle)),'+',sin(deg2rad(angle)))
+		print('Velocity:', velocity.x,'+' ,velocity.z)
 		#velocity.y = 20
