d3b7477bcaad3e74fede6637427dca83c015b2d9
Abiral Pokharel <22109745+abiral001@users.noreply.github.com>
2020-04-16 13:04:23 +0545

    Initial fixes

--- cart.gd
+++ cart.gd
@@ -1,6 +1,9 @@
 extends KinematicBody
 
 var direction = Vector3()
+var speed = 200
+var velocity = Vector3()
+var g = -9.8
 
 func _physics_process(delta):
 	direction = Vector3(0,0,0)
@@ -13,5 +16,18 @@ func _physics_process(delta):
 	if Input.is_key_pressed(KEY_RIGHT):
 		direction.x += 10
 	direction = direction.normalized()
-	move_and_collide(direction)
-	#move_and_slide(direction, Vector3(0,1,0))
+	direction = direction * speed
+	velocity.y += g*delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+	if velocity.y > 0:
+		g = -29.4
+	else:
+		g = -9.8
+	
+	#move_and_collide(velocity)
+	velocity = move_and_slide(velocity, Vector3(0,-1,0))
+		
+	if Input.is_key_pressed(KEY_SPACE):
+		velocity.y = 20
+	
